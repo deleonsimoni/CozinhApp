ab9f1ae596fc362a7699c777706f249b077cf5a3
iuriian <iuriian@hotmail.com>
2019-03-23 04:07:16 -0300

    ajustes de layout

--- src/app/selecionarIngredientes/selecionarIngredientes.page.ts
+++ src/app/selecionarIngredientes/selecionarIngredientes.page.ts
@@ -1,61 +1,78 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { trigger, style, state, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'selecionarIngredientes.page.html',
   styleUrls: ['selecionarIngredientes.page.scss'],
+  // animations: [
+  //   trigger('manipularItem', [
+  //     state('selecionar', style({
+  //       backgroundColor: 'green',
+  //       color: '#fff',
+  //       opacity: 1
+  //     })),
+  //     state('excluir', style({
+  //       backgroundColor: 'red',
+  //       color: '#fff',
+  //       opacity: 1
+  //     })),
+  //     transition(':leave', [
+  //       style({ opacity: 1 }),
+  //       animate(500, style({ opacity: 0 }))
+  //     ]),
+  //   ])
+  // ]
 })
 export class SelecionarIngredientes {
 
-  public ingredientes:any[] = [];
-  public filtroIngredientes:any[] = [];
-  public ingredienteSelecionados:any[] = [];
-  public parametroPesquisa:string = "";
-
-  public errorMsg;
+  public ingredientes: any[] = [];
+  public filtroIngredientes: any[] = [];
+  public ingredienteSelecionados: any[] = [];
+  public parametroPesquisa = '';
+  public errorMsg: any;
+  item: string = '';
 
   constructor(private http: HttpClient) {
 
     this.getIngredientes();
 
   }
-  
-  getIngredientes(){
-        const httpJson = this.http.get<any>("././assets/jsons/ingredientes.json")
-        
-        httpJson.subscribe(data => {
-                                      this.ingredientes = data.ingredientes;
-                                    },
 
-                           error => { 
-                                      this.errorMsg = error;
-                                    }
-                          )
+  getIngredientes(){
+    const httpJson = this.http.get<any>('././assets/jsons/ingredientes.json');
+
+    httpJson.subscribe(data => {
+      this.ingredientes = data.ingredientes;
+    }, error => {
+      this.errorMsg = error;
+    });
   }
 
   filtrarIngredientes(param){
     this.parametroPesquisa = param;
-    if(param == ""){
+    if (param === '') {
       this.filtroIngredientes = [];
     } else {
       this.filtroIngredientes = this.ingredientes.filter(element  => {
         return element.nome.toLowerCase().startsWith(param.toLowerCase());
       });
-
     }
   }
 
   selecionarIngrediente(ingrediente, index){
+    this.item = 'selecionar';
     this.ingredienteSelecionados.push(ingrediente);
     this.filtroIngredientes.splice(index, 1);
     this.ingredientes.splice(index, 1);
   }
 
-  removerIngrediente(ingrediente, index){
-    this.ingredientes.splice(ingrediente.id-1,0,ingrediente);
-    if(ingrediente.nome.toLowerCase().startsWith(this.parametroPesquisa.toLowerCase())){
-      this.filtroIngredientes.splice(index,0,ingrediente);
+  removerIngrediente(ingrediente, index) {
+    this.item = 'excluir';
+    this.ingredientes.splice(ingrediente.id - 1, 0, ingrediente);
+    if (ingrediente.nome.toLowerCase().startsWith(this.parametroPesquisa.toLowerCase())) {
+      this.filtroIngredientes.splice(index, 0, ingrediente);
     }
     this.ingredienteSelecionados.splice(index, 1);
   }
