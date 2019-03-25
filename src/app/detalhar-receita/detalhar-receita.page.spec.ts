9c66f2a5c81b65fb8ab2a975e8b3adc0c510297e
DeleonxD <deleonsimoni@gmail.com>
2019-03-25 02:00:38 -0300

    evolução de páginas e acertos de layout

--- src/app/detalhar-receita/detalhar-receita.page.spec.ts
+++ src/app/detalhar-receita/detalhar-receita.page.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import { DetalharReceitaPage } from './detalhar-receita.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('DetalharReceitaPage', () => {
+  let component: DetalharReceitaPage;
+  let fixture: ComponentFixture<DetalharReceitaPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ DetalharReceitaPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(DetalharReceitaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
